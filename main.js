79bceadc090b88e4871666efb016a9743854300e
yasmin1710 <144241333+yasmin1710@users.noreply.github.com>
2025-06-06 11:53:12 -0300

    salva

---
+++ main.js
@@ -0,0 +1,25 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('parametro-senha__botao');
+
+botoes[0].onclick = diminuitamanho;
+botoes[1].onclick = aumentatamanho;
+
+function diminuitamanho(){
+    if (tamanhoSenha > 1){
+        //tamanhoSenha = tamanhosenha-1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+        //tamanhoSenha = tamanhoSenha+1;
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+console.log(botoes);
